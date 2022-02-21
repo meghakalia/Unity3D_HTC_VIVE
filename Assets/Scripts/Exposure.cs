13baa202745042b17c91e4b0abaa62048dfefa08
Megha Kalia <kalia.megha84@gmail.com>
2022-02-21 16:44:46 -0500

    code to blink an LED with certain duration, attribute to reset the LED duration

--- Assets/Scripts/Exposure.cs
+++ Assets/Scripts/Exposure.cs
@@ -4,36 +4,62 @@ using UnityEngine;
 
 public class Exposure : MonoBehaviour
 {
-    //Renderer mLED_object ;
-
-    //IEnumerator LEDFlash()
-    //{
-    //    Color c = mLED_object.material.color;
-    //    for (float alpha = 1f; alpha >= 0; alpha -= 0.1f)
-    //    {
-    //        c.a = alpha;
-    //        renderer.material.color = c;
-    //        yield return null;
-    //    }
-    //}
-    public Material m_Material;
-
-
+    
+    //public Material m_Material;
+    bool firstTime            = true;
+    float prevTime            = 0;
+    public float timeDelay    = 0 ;
+    //[System.NonSerialized]
+    public float timePeriod   = 1.0f;
+    float timeParsed          = 0;
+    bool b_lightOn            = true; 
     // Start is called before the first frame update
     void Start()
     {
-        m_Material = GetComponent<Renderer>().material;
-        //m_Material.EnableKeyword("_EMISSION");
-        m_Material.DisableKeyword("_EMISSION");
-        //mLED_object = GetComponent<Renderer>();
+        //m_Material = GetComponent<Renderer>().material;
+        //m_Material.DisableKeyword("_EMISSION");
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    //Time.fixedDeltaTime
+        //    GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+        //}
+
+        if (firstTime)
         {
-            GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+            prevTime = Time.time;
+            firstTime = !firstTime;
         }
+
+        if ((Time.time - prevTime) > timeDelay)
+        {
+            ////run for 1 second
+            //GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+            timeParsed = timeParsed + Time.deltaTime;
+
+            if (timeParsed >= timePeriod)
+            {
+                if (b_lightOn == false)
+                {
+                    GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+                    b_lightOn = !b_lightOn;
+                    Debug.Log(" light on : Time parsed %f  " + timeParsed); 
+                }
+                else
+                {
+                    GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+                    b_lightOn = !b_lightOn;
+                    Debug.Log("light off - Time parsed %f " + timeParsed);
+                }
+
+                timeParsed = 0;
+            }
+        }
+
     }
 }
